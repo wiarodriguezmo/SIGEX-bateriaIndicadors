1987e7ea137c49ae3929996db3197e36e7eb9142
wiarodriguezmo <wiarodriguezmo@unal.edu.co>
2015-12-11 09:43:30 -0500

    Entrega Diciembre

--- app/initialCtrl.js
+++ app/initialCtrl.js
@@ -18,21 +18,21 @@ app.controller('initialCtrl', function ($scope, Data) {
         $scope.indicadoresS = evaluaSec($scope.secundaria);
     });
     evaluaSec = function(sec){
-      var uno = sec.uno>10?"btn-success":sec.uno>4?"btn-warning":"btn-danger";
-      var dos = sec.dos>0.3?"btn-success":sec.dos>=0.15?"btn-warning":"btn-danger";
-      var tres = sec.tres>0.3?"btn-success":sec.tres>=0.15?"btn-warning":"btn-danger";
-      var cuatro = sec.cuatro>0.3?"btn-success":sec.cuatro>=0.15?"btn-warning":"btn-danger";
-      var diez = sec.diez>1?"btn-success":sec.diez=1?"btn-warning":"btn-danger";
-      var quince = sec.quince>0.8?"btn-success":sec.quince>=0.6?"btn-warning":"btn-danger";
-      var dieciocho = sec.dieciocho>6?"btn-success":sec.dieciocho>=4?"btn-warning":"btn-danger";
-      var veintiseis = sec.veintiseis>0.8?"btn-success":sec.veintiseis>=0.6?"btn-warning":"btn-danger";
-      var veintiocho = sec.veintiocho>11?"btn-success":sec.veintiocho>=6?"btn-warning":"btn-danger";
-      var veintinueve = sec.veintinueve>0.8?"btn-success":sec.veintinueve>=0.6?"btn-warning":"btn-danger";
-      var treinta = sec.treinta>1?"btn-success":sec.treinta=1?"btn-warning":"btn-danger";
-      var treintaycinco = sec.treintaycinco>=4000000000?"btn-success":sec.treintaycinco>=2000000000?"btn-warning":"btn-danger";
-      var treintayseis = sec.treintayseis>0.8?"btn-success":sec.treintayseis>=0.6?"btn-warning":"btn-danger";
-      var treintaysiete = sec.treintaysiete>0.8?"btn-success":sec.treintaysiete>=0.6?"btn-warning":"btn-danger";
-      var cuarentaytres = sec.cuarentaytres>0.8?"btn-success":sec.cuarentaytres>=0.6?"btn-warning":"btn-danger";
+      var uno = sec.uno>10?"btn-success":sec.uno>4?"btn-warning":sec.uno==-1?"btn-danger nd":"btn-danger";
+      var dos = sec.dos>0.3?"btn-success":sec.dos>=0.15?"btn-warning":sec.dos==-1?"btn-danger nd":"btn-danger";
+      var tres = sec.tres>0.3?"btn-success":sec.tres>=0.15?"btn-warning":sec.tres==-1?"btn-danger nd":"btn-danger";
+      var cuatro = sec.cuatro>0.3?"btn-success":sec.cuatro>=0.15?"btn-warning":sec.cuatro==-1?"btn-danger nd":"btn-danger";
+      var diez = sec.diez>1?"btn-success":sec.diez==1?"btn-warning":sec.diez==-1?"btn-danger nd":"btn-danger";
+      var quince = sec.quince>0.8?"btn-success":sec.quince>=0.6?"btn-warning":sec.quince==-1?"btn-danger nd":"btn-danger";
+      var dieciocho = sec.dieciocho>6?"btn-success":sec.dieciocho>=4?"btn-warning":sec.dieciocho==-1?"btn-danger nd":"btn-danger";
+      var veintiseis = sec.veintiseis>0.8?"btn-success":sec.veintiseis>=0.6?"btn-warning":sec.veintiseis==-1?"btn-danger nd":"btn-danger";
+      var veintiocho = sec.veintiocho>11?"btn-success":sec.veintiocho>=6?"btn-warning":sec.veintiocho==-1?"btn-danger nd":"btn-danger";
+      var veintinueve = sec.veintinueve>0.8?"btn-success":sec.veintinueve>=0.6?"btn-warning":sec.veintinueve==-1?"btn-danger nd":"btn-danger";
+      var treinta = sec.treinta>1?"btn-success":sec.treinta==1?"btn-warning":sec.treinta==-1?"btn-danger nd":"btn-danger";
+      var treintaycinco = sec.treintaycinco>=4000000000?"btn-success":sec.treintaycinco>=2000000000?"btn-warning":sec.treintaycinco==-1?"btn-danger nd":"btn-danger";
+      var treintayseis = sec.treintayseis>0.8?"btn-success":sec.treintayseis>=0.6?"btn-warning":sec.treintayseis==-1?"btn-danger nd":"btn-danger";
+      var treintaysiete = sec.treintaysiete>0.8?"btn-success":sec.treintaysiete>=0.6?"btn-warning":sec.treintaysiete==-1?"btn-danger nd":"btn-danger";
+      var cuarentaytres = sec.cuarentaytres>0.8?"btn-success":sec.cuarentaytres>=0.6?"btn-warning":sec.cuarentaytres==-1?"btn-danger nd":"btn-danger";
 
       return { "uno":uno,"dos":dos,"tres":tres,"cuatro":cuatro,"diez":diez,"quince":quince,"dieciocho":dieciocho,"veintiseis":veintiseis,"veintiocho":veintiocho,"veintinueve":veintinueve,"treinta":treinta,"treintaycinco":treintaycinco,"treintayseis":treintayseis,"treintaysiete":treintaysiete,"cuarentaytres":cuarentaytres };
     };
